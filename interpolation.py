a428bd3d60c354a8296c1b5827a5a926f136960c
517030910405 <42196261+517030910405@users.noreply.github.com>
2020-08-07 20:26:38 +0800

    Add files via upload

--- interpolation.py
+++ interpolation.py
@@ -15,6 +15,7 @@ def function1():
     plt.plot(x, y, 'o', xnew, f(xnew), '-', xnew, f2(xnew), '--')
     plt.legend(['data', 'linear', 'cubic'], loc='best')
     plt.savefig("tmp.png")
+    
 def function2():
     x = np.arange(0, 2*np.pi+np.pi/4, 2*np.pi/8)
     y = np.sin(x)
@@ -30,6 +31,7 @@ def function2():
     plt.title('Cubic-spline interpolation')
     plt.show()
     plt.savefig("tmp2.png")
+
 def fit(GT):
     t = np.arange(GT.shape[0],dtype=np.float64)*.1
     t_new = np.arange((GT.shape[0]-1)*10+1,dtype=np.float64)*.01
@@ -80,10 +82,8 @@ def demo_interpolation(GT, accel, vel, pose):
         GT[0,:3]]
     )
 
-
     accel_sth, vel_sth, pose_sth = integrate_smooth(accel,init_motion=init_motion_interpolation,dt=1e-2)
     accel_, vel_, pose_ = integrate(accel,init_motion=init_motion_interpolation,dt=1e-2)
-
 
     plt.figure()
     plt.plot(pose[0], pose[1],'-', GT[:,0], GT[:,1],'--')
@@ -121,7 +121,6 @@ def demo_interpolation(GT, accel, vel, pose):
     plt.title('accel')
     plt.savefig("accel_compare_.png")
     
-
 def demo(GT, accel, vel, pose, rt = 'img2/'):
     t = np.arange(GT.shape[0],dtype=np.float64)*.1
     t_new = np.arange((GT.shape[0]-1)*10+1,dtype=np.float64)*.01
@@ -147,14 +146,11 @@ def demo(GT, accel, vel, pose, rt = 'img2/'):
         GT[0,:3]]
     )
 
-
     accel_sth, vel_sth, pose_sth = integrate_smooth(accel,init_motion=init_motion_interpolation,dt=1e-2)
     accel_, vel_, pose_ = integrate(accel,init_motion=init_motion_interpolation,dt=1e-2)
 
     accel_sth_resim, vel_sth_resim, pose_sth_resim = integrate_smooth(accel_simplex,init_motion=init_motion_simplex,dt=1e-2)
     accel_resim, vel_resim, pose_resim = integrate(accel_simplex,init_motion=init_motion_simplex,dt=1e-2)
-
-
 
     plt.figure()
     plt.plot(pose[0], pose[1],'-', GT[:,0], GT[:,1],'--')
@@ -167,7 +163,6 @@ def demo(GT, accel, vel, pose, rt = 'img2/'):
     plt.legend(['Interpolation-reintegral-simple integration','Interpolation-reintegral-spline during integration', 'True'])
     plt.title('interpolation')
     plt.savefig(rt+"interpolation_reintegral.png")
-
 
     plt.figure()
     plt.plot(pose_resim[0], pose_resim[1],'-',pose_sth_resim[0], pose_sth_resim[1],'-', GT[:,0], GT[:,1],'--')
@@ -213,17 +208,16 @@ def demo(GT, accel, vel, pose, rt = 'img2/'):
     plt.title('velocity after re-integration')
     plt.savefig(rt+"vel_compare_re-integration.png")
 
-
     plt.figure()
     plt.plot(t_new, np.linalg.norm(accel,axis=0),'--', t_simplex, np.linalg.norm(accel_simplex,axis=0),'--')
     plt.legend(['Interpolation', 'Simplex'])
     plt.title('accel')
     plt.savefig(rt+"accel_compare_direct.png")
     
-    pass
 def main():
     GT = np.loadtxt("pose_left.txt",delimiter=" ")
     accel,vel,pose = fit(GT)
     demo(GT,accel,vel,pose)
-# function2()
-main()
+    
+if (__name__ == '__main__'):
+    main()
